737c2d43a6b1cefb022db559817cf03f92da902a
Andrew <70753182+IlinAndrey@users.noreply.github.com>
2021-11-20 13:44:50 +0300

    Delete header2.php

--- header2.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Web-chat</title>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/styleLogin.css">
-    <script src="https://use.fontawesome.com/8aab69e184.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-</head>
